2f7dac95602918c8008f90487383524ef8065829
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-09-06 11:20:39 +0900

    テスト、デバグ完了

--- no1/no1.pl
+++ no1/no1.pl
@@ -8,8 +8,11 @@ use feature qw(switch say);
 # ピン数インプット
 my $file = $ARGV[0];
 open my $in, "<", $file or die "ファイルが見つかりません";
-my $throw_num = chomp(<$in>);
-my @throws = split(/\s/,chomp(<$in>)); 
+my $throw_num = <$in>;
+chomp ($throw_num);
+my $throw_line = <$in>;
+chomp ($throw_line);
+my @throws = split(/\s/,$throw_line); 
 close $in;
 
 die "投げた回数があいません" if ($throw_num != @throws);
@@ -19,28 +22,28 @@ my $index = 0;
 foreach my $inning (0..9) {
     my ($score, $next) = score(\@throws,$index);
     $scores[$inning] = $score;
-    $index = next;
+    $index = $next;
 }
 my $total = 0;
-$total + $_ for @scores;
+$total += $_ for @scores;
 
 say $total;
 
-# X+1投目の倒れた数を得る。足りなければ例外
+# $index+1投目の倒れた数を得る。足りなければ例外
 sub fetch {
     my $throws = shift;
     my $index = shift;
     die "投げた数が足りません" if ($index >= @{$throws});
     my $ret = $throws->[$index];
-    die "0-10の数字を入れてください" unless ($index =~ /^(?:10|\d)$/);
+    die "0-10の数字を入れてください" unless ($ret =~ /^(?:10|\d)$/);
     return $ret;
 }
 
-# レーンのスコアと次の投目を計算。そのレーンが何投目から始まるかはY+1で指定。
+# レーンのスコアと次の投目を計算。そのレーンが何投目から始まるかは$index+1で指定。
 sub score {
     my $throws = shift;
     my $index = shift;
-    my $score = $first = fetch($throws,$index);
+    my $score = my $first = fetch($throws,$index);
     $score += fetch($throws,++$index);
     if ($first == 10) {
         $score += fetch($throws,$index+1);
