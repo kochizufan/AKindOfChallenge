9db3dd65355710b968897d66f6572c7b30b8fb33
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-09-06 13:01:21 +0900

    実装完了、デバグ移行

---
+++ no3/no3.pl
@@ -0,0 +1,47 @@
+#!perl
+
+use strict;
+use warnings;
+
+use feature qw(switch say);
+use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
+use Crypt::CBC;
+
+my ($source,$destination,$inverse,$key);
+$inverse = 0;
+$key = "default key";
+GetOptions(
+    "source|s=s" => \$source,
+    "destination|d=s" => \$destination,
+    "inverse|" => \$inverse,
+    "key|k:s" => \$key
+);
+
+die "元ファイルがありません" unless (-e $source);
+
+# 元ファイル内容読み込み
+open my $src_fd,"<",$source;
+my $input = '';
+while (my $line = <$src_fd>) {
+    $input .= $line;
+}
+close $src_fd;
+
+my $cipher = Crypt::CBC->new( 
+    -key    => $key,
+    -cipher => 'Blowfish'
+);
+
+my $output;
+if ($inverse) {
+    $output = $cipher->decrypt($input);
+} else {
+    $output = $cipher->encrypt($input);
+}
+
+open my $dst_fd,">",$destination;
+print $dst_fd $output;
+close $dst_fd;
+
+
+
